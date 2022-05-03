2e083e6c4baa08d072cfed4ba5df54d38c6ccbea
Ruslan <6r0d5k1y@gmail.com>
2022-05-03 23:47:25 +0300

    корректировка комментариев

--- control.py
+++ control.py
@@ -1,16 +1,16 @@
 import constants
 
-def take_input(symbol):
+def take_input(symbol):#вывод либо крестикка либо нолика
     # 1.функция принимает от пользователя то что он ставит
     # 2.определяет что он ставит,
     # 3.определяет не занята ли клетка
     while True:
         value = input('куда поставить ' + symbol + '? ')  # запятая не проходит , только плюс
         if not (value in '123456789'):
-            print('ошибочный ввод')
-            continue
-        value = int(value)
-        if str(constants.board[value - 1]) in 'XO':
+            print('некорректный ввод, введите цифру от 1 до 9 на не занятую позицию')
+            continue # возвращение выполнения цикла в начало цикла
+        value = int(value) # преобразование значения к целому типу
+        if str(constants.board[value - 1]) in 'XO': #предупреждение некорректного ввода
             print('эта клетка уже занята')
             continue
         constants.board[value - 1] = symbol
@@ -19,9 +19,9 @@ def take_input(symbol):
 
 def check_win():
     """проверка на выигрыш"""
-    for each in constants.wins_coord:  # если нет условия окончания цикла (break, continue, return),
+    for i in constants.wins_coord:  # если нет условия окончания цикла (break, continue, return),
         # то цикл переходит на else
-        if (constants.board[each[0] - 1]) == (constants.board[each[1] - 1]) == (constants.board[each[2] - 1]):
-            return constants.board[each[1] - 1]
+        if (constants.board[i[0] - 1]) == (constants.board[i[1] - 1]) == (constants.board[i[2] - 1]):
+            return constants.board[i[1] - 1]
     else:  # не для условия а для цикла
         return False
